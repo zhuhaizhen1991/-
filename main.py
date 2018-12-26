c19eaf70e757115f1c1f7557ee74d32003638781
zhuhaizhen <zhuhaizhen1991@163.com>
2018-12-26 21:39:16 +0800

    添加session存储

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from datetime import timedelta
+from flask import Flask, session
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from redis import Redis
 
@@ -11,18 +13,26 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False #是否追踪数据库变化
     REDIS_HOST = '127.0.0.1' #redis的连接地址 将自定义的配置也封装到config类中
     REDIS_PORT = 6379
+    SESSION_TYPE = 'redis'#设置session存储的方式,redis性能好,方便设置过期时间
+    SESSION_REDIS = Redis(host=REDIS_HOST,port=REDIS_PORT)#设置redis链接对象,组件会使用该对象将session数据保存到redis中
+    SESSION_USE_SIGNER = True #设置sessionid进行加密
+    SECRET_KEY = 'test'#设置sessionid秘钥
+    PERMANENT_SESSION_LIFETIME = timedelta(days=7)
 
 #从对象中加载配置
 app.config.from_object(Config)
-
 
 #创建mysql连接对象
 db = SQLAlchemy(app)
 #创建redis连接对象
 rs = Redis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
 
+#初始化session存储对象
+Session(app)
+
 @app.route('/index')
 def index():
+    session['name'] = 'lisi'
     return '首页'
 
 if __name__ == '__main__':
